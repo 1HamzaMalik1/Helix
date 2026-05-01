1b038dd1cf12b5185da851712e85046bb6f18a03
Hamza Malik <hamzahakim12235@gmail.com>
2026-05-01 16:25:47 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,14 @@ export const metadata: Metadata = {
   },
 };
 
+const GTM_ID = 'GTM-MM5DXNVF';
+
+const gtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`;
+
 const structuredData = {
   '@context': 'https://schema.org',
   '@graph': [
@@ -113,6 +121,20 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className={`${poppins.className} bg-white text-primary overflow-x-hidden antialiased`}>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height={0}
+            width={0}
+            style={{ display: 'none', visibility: 'hidden' }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        <Script
+          id="gtm"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{ __html: gtmScript }}
+        />
         <Script
           id="schema"
           type="application/ld+json"
